78d0a3e5c29cfba8cbe6208933e60a2e9b9a216f
JSpencerPittman <jspencerpittman@gmail.com>
2024-09-12 13:01:38 -0500

    Support multiple layers

--- raindrop/raindrop.py
+++ raindrop/raindrop.py
@@ -16,6 +16,7 @@ class Raindrop(nn.Module):
                  pe_emb_dim: int,
                  timesteps: int,
                  out_dim: int,
+                 num_layers: int = 2,
                  dropout: float = 0.3,
                  init_range: float = 1e-10,
                  device: str = 'cpu',
@@ -30,6 +31,7 @@ class Raindrop(nn.Module):
         self.pe_emb_dim = pe_emb_dim
         self.timesteps = timesteps
         self.out_dim = out_dim
+        self.num_layers = num_layers
         self.dropout = nn.Dropout(dropout)
         self.init_range = init_range
         self.device = device
@@ -44,9 +46,9 @@ class Raindrop(nn.Module):
 
         # Inter-Sensor Attention
         self.inter_sensor_attn_weights = nn.Parameter(
-            torch.zeros(num_sensors, self.inter_sensor_attn_dim)
+            torch.zeros(num_layers, num_sensors, self.inter_sensor_attn_dim)
         )
-        self.inter_sensor_recv_map = nn.Linear(self.obs_embed_dim,
+        self.inter_sensor_proj_map = nn.Linear(self.obs_embed_dim,
                                                self.inter_sensor_attn_dim + pe_emb_dim)
         self.inter_sensor_bidir_weights = nn.Parameter(
             torch.zeros(self.num_sensors, self.obs_embed_dim)
@@ -73,15 +75,15 @@ class Raindrop(nn.Module):
         # Exclude batch_size
         h = self._embed_observation(x) * mask
         pe = self.pos_encoder(times)
+        adj = self._init_adjacency_graph(batch_size)
 
-        alpha = self._calc_inter_sensor_attn_weights(h, pe)
-        adj_l1 = self._init_adjacency_graph(batch_size)
+        for lay_idx in range(self.num_layers):
+            alpha = self._calc_inter_sensor_attention(h, pe, lay_idx)
 
-        h = self._propagate_message(h, alpha, adj_l1)
+            if lay_idx:
+                adj = self._calc_next_adj_graph_layer(adj, alpha, mask)
 
-        adj_l2 = self._init_next_layer(adj_l1, alpha, mask)
-
-        h = self._propagate_message(h, alpha, adj_l2)
+            h = self._propagate_message(h, alpha, adj)
 
         H = torch.concat([h, pe.unsqueeze(2).repeat(1, 1, self.num_sensors, 1)], dim=-1)
         H = H.permute(0, 2, 1, 3)
@@ -94,18 +96,20 @@ class Raindrop(nn.Module):
                            ) -> tt.BatTimeSenObs_EmbTensor:
         return F.relu(torch.einsum("ijkl,klm->ijkm", x, self.obs_emb_weights))
 
-    def _calc_inter_sensor_attn_weights(self,
-                                        h: tt.BatTimeSenObs_EmbTensor,
-                                        pe: tt.BatTimePeTensor,
-                                        ) -> tt.BatTimeSenSenTensor:
+    def _calc_inter_sensor_attention(self,
+                                     h: tt.BatTimeSenObs_EmbTensor,
+                                     pe: tt.BatTimePeTensor,
+                                     lay_idx: int,
+                                     ) -> tt.BatTimeSenSenTensor:
         batch_size, time_steps, _, _ = h.shape
         pe = pe.unsqueeze(2).repeat(1, 1, self.num_sensors, 1)
-        attn_weights = \
-            self.inter_sensor_attn_weights[None, None, :, :].repeat(batch_size,
-                                                                    time_steps,
-                                                                    1, 1)
+        attn_weights = self.inter_sensor_attn_weights[lay_idx]
+        attn_weights = attn_weights[None, None, :, :].repeat(batch_size,
+                                                             self.timesteps,
+                                                             1,
+                                                             1)                                                 1, 1)
         alpha = torch.concat([attn_weights, pe], dim=-1)
-        h = self.inter_sensor_recv_map(h)
+        h = self.inter_sensor_proj_map(h)
 
         alpha = h @ alpha.T
         return F.relu(alpha)
@@ -129,10 +133,10 @@ class Raindrop(nn.Module):
                           self.num_sensors,
                           device=self.device)
 
-    def _init_next_layer(self,
-                         adj_prev: tt.BatSenSenTensor,
-                         alpha: tt.BatTimeSenSenTensor,
-                         mask: tt.BatTimeSenTensor) -> tt.BatSenSenTensor:
+    def _calc_next_adj_graph_layer(self,
+                                   adj_prev: tt.BatSenSenTensor,
+                                   alpha: tt.BatTimeSenSenTensor,
+                                   mask: tt.BatTimeSenTensor) -> tt.BatSenSenTensor:
         return adj_prev * (alpha.sum(1) / mask.sum(1).unsqueeze(-1))
 
     def _calc_temporal_self_attention(self, H: tt.BatSenTimeObs_Pe_EmbTensor
