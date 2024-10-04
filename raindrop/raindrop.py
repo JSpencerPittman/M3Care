Added more gradient analysis tools and fixed Dataloader

raindrop/raindrop.py
@@ -48,10 +48,10 @@ class Raindrop(nn.Module):
         # Observation Embedding
         assert obs_embed_dim % num_heads == 0
 
+        self.scale_inp_bn = nn.BatchNorm2d(timesteps)
         self.obs_emb_weights = nn.Parameter(
             torch.zeros(num_sensors, obs_dim, obs_embed_dim * num_heads)
             )
-        self.obs_emb_bn = nn.BatchNorm3d(num_heads)
         self.pos_encoder = RaindropPositionalEncoder(pe_emb_dim, device=device)
 
         # Inter-Sensor Attention
@@ -60,8 +60,6 @@ class Raindrop(nn.Module):
         )
         self.inter_sensor_proj_map = nn.Linear(self.obs_embed_dim,
                                                self.inter_sensor_attn_dim + pe_emb_dim)
-        self.inter_sensor_proj_bn = \
-            nn.BatchNorm3d(num_heads)
         self.inter_sensor_bidir_weights = nn.Parameter(
             torch.zeros(num_layers, num_heads, num_sensors, self.inter_sensor_attn_dim))
 
@@ -71,9 +69,12 @@ class Raindrop(nn.Module):
         self.temp_attn_key_map = nn.Linear(obs_embed_dim + pe_emb_dim,
                                            self.temp_attn_dim)
         self.temp_attn_s_map = nn.Linear(timesteps, 1)
+        self.temp_attn_softmax = nn.Softmax(dim=-1)
+        self.temp_attn_bn = nn.LayerNorm((num_sensors, timesteps))
 
         # Sensor Embeddings
         assert out_dim % num_heads == 0
+        self.sensor_embed_ln = nn.LayerNorm((num_sensors, out_dim // num_heads))
         self.sensor_embed_map = nn.Linear(obs_embed_dim + pe_emb_dim,
                                           out_dim // num_heads)
 
@@ -84,7 +85,7 @@ class Raindrop(nn.Module):
                 times: tt.BatTimeTensor,
                 mask: tt.BatTimeSenTensor):
         batch_size = x.shape[0]
-
+        x = self.scale_inp_bn(x)
         h = self._embed_observation(x) * mask[:, None, :, :, None]
         pe = self.pos_encoder(times)
         adj = self._init_adj_graph(batch_size)
@@ -121,7 +122,7 @@ class Raindrop(nn.Module):
                    self.num_heads,
                    self.obs_embed_dim
                    ).permute(0, 3, 1, 2, 4)
-        return self.obs_emb_bn(h)
+        return h
 
     def _calc_inter_sensor_attention(self,
                                      h: tt.BatHeadTimeSenObs_EmbTensor,
@@ -140,7 +141,6 @@ class Raindrop(nn.Module):
 
         alpha = torch.concat([attn_weights, pe], dim=-1).swapaxes(-1, -2)
         h = self.inter_sensor_proj_map(h)
-        h = self.inter_sensor_proj_bn(h)
 
         return F.leaky_relu(h @ alpha)
 
@@ -192,23 +192,27 @@ class Raindrop(nn.Module):
                                       ) -> tt.BatHeadSenTimeTensor:
         Q = self.temp_attn_query_map(H)
         K = self.temp_attn_key_map(H)
-
-        norm = math.sqrt(self.obs_embed_dim + self.pe_emb_dim)
-        beta = (Q @ K.swapaxes(-1, -2)) / norm
+        # norm = math.sqrt(self.obs_embed_dim + self.pe_emb_dim)
+        beta = (Q @ K.swapaxes(-1, -2))  # / norm
         beta = self.temp_attn_s_map(beta).squeeze(dim=-1)
-        return F.softmax(beta, dim=-1)
+        beta = self.temp_attn_bn(beta)
+        # return self.temp_attn_softmax(beta)
+        return beta
 
     def _generate_sensor_embedding(self,
                                    H: tt.BatHeadSenTimeObs_Pe_EmbTensor,
                                    beta: tt.BatHeadSenTimeTensor
                                    ) -> tt.BatSenOutTensor:
         batch_size = H.shape[0]
+
         out = F.leaky_relu((beta.unsqueeze(-1) * H).sum(3))
         out = F.leaky_relu(self.sensor_embed_map(out))
+        out = self.sensor_embed_ln(out)
         out = out.permute(0, 2, 1, 3)
+
         return torch.reshape(out, (batch_size, self.num_sensors, self.out_dim))
 
     def _init_weights(self):
-        nn.init.xavier_uniform_(self.obs_emb_weights)
-        nn.init.xavier_uniform_(self.inter_sensor_attn_weights)
-        nn.init.xavier_uniform_(self.inter_sensor_bidir_weights)
+        nn.init.kaiming_uniform_(self.obs_emb_weights)
+        nn.init.kaiming_uniform_(self.inter_sensor_attn_weights)
+        nn.init.kaiming_uniform_(self.inter_sensor_bidir_weights)

util/grad_track.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from typing import Optional
+
 from torch import Tensor, nn
 
 
@@ -13,7 +16,9 @@ class SubmoduleGradientTracker(object):
         self.in_grad_is_none: list[bool] = [False] * num_inp
         self.out_grad_is_none: list[bool] = [False] * num_out
 
-    def track(self, grad_inp: tuple[Tensor], grad_out: tuple[Tensor]):
+    def track(self,
+              grad_inp: tuple[Optional[Tensor]],
+              grad_out: tuple[Optional[Tensor]]):
         assert self.num_inp == len(grad_inp)
         assert self.num_out == len(grad_out)
 
@@ -30,20 +35,114 @@ class SubmoduleGradientTracker(object):
                 self.out_grad_is_none[out_idx] = True
 
 
+class ParamGradientTracker(object):
+    def __init__(self, name: str):
+        self.name = name
+        self.grad_norms: list[float] = []
+        self.is_none = False
+
+    def track(self, grad: Optional[Tensor]):
+        if grad is None:
+            self.is_none = True
+            self.grad_norms.append(None)
+        else:
+            self.grad_norms.append(grad.norm().item())
+
+
 class GradientTracker(object):
     def __init__(self, model: nn.Module):
         self.tracked_modules: list[str] = []
-        self.trackers: dict[str, SubmoduleGradientTracker] = {}
+        self.tracked_params: list[str] = []
+
+        self.module_trackers: dict[str, SubmoduleGradientTracker] = {}
+        self.param_trackers: dict[str, ParamGradientTracker] = {}
 
         for name, module in model.named_modules():
             self.tracked_modules.append(name)
-            module.register_full_backward_hook(self._create_backward_hook(name))
+            module.register_full_backward_hook(self._create_module_backward_hook(name))
 
-    def _create_backward_hook(self, name: str):
+        for name, param in model.named_parameters():
+            self.tracked_params.append(name)
+            param.register_hook(self._create_parameter_backward_hook(name))
+
+    def _create_module_backward_hook(self, name: str):
         def hook(module: nn.Module, grad_inp: tuple[Tensor], grad_out: tuple[Tensor]):
-            if name not in self.trackers:
-                self.trackers[name] = SubmoduleGradientTracker(name,
-                                                               len(grad_inp),
-                                                               len(grad_out))
-            self.trackers[name].track(grad_inp, grad_out)
+            if name not in self.module_trackers:
+                self.module_trackers[name] = SubmoduleGradientTracker(name,
+                                                                      len(grad_inp),
+                                                                      len(grad_out))
+            self.module_trackers[name].track(grad_inp, grad_out)
         return hook
+
+    def _create_parameter_backward_hook(self, name: str):
+        def hook(grad: Optional[Tensor]):
+            if name not in self.param_trackers:
+                self.param_trackers[name] = ParamGradientTracker(name)
+            self.param_trackers[name].track(grad)
+        return hook
+
+
+class GradientFlowAnalyzer(object):
+    class StepType(Enum):
+        ModuleIn = 0
+        ModuleOut = 1
+        Parameter = 2
+
+    Flow = list[tuple[StepType, float]]
+
+    def __init__(self, gt: GradientTracker):
+        self.gt = gt
+
+    def flow(self,
+             order: list[str | tuple[str, int, int]],
+             steps: int | list[int]
+             ) -> dict[int, Flow]:
+        flow: dict[int, list[(GradientFlowAnalyzer.StepType, float)]] = {}
+
+        if isinstance(steps, int):
+            steps = [steps]
+
+        for step in steps:
+            flow[step] = []
+            for part in order:
+                if isinstance(part, tuple) or (part in self.gt.tracked_modules):
+                    if isinstance(part, str):
+                        name, in_idx, out_idx = part, 0, 0
+                    else:
+                        name, in_idx, out_idx = part
+
+                    tracker = self.gt.module_trackers[name]
+                    if not tracker.out_grad_is_none[out_idx]:
+                        flow[step].append((name,
+                                           GradientFlowAnalyzer.StepType.ModuleOut,
+                                           tracker.grad_out_norms[out_idx][step]))
+                    if not tracker.in_grad_is_none[in_idx]:
+                        flow[step].append((name,
+                                           GradientFlowAnalyzer.StepType.ModuleIn,
+                                           tracker.grad_in_norms[in_idx][step]))
+                else:
+                    name = part
+                    tracker = self.gt.param_trackers[name]
+                    if not tracker.is_none:
+                        flow[step].append((name,
+                                           GradientFlowAnalyzer.StepType.Parameter,
+                                           tracker.grad_norms[step]))
+        return flow
+
+    @staticmethod
+    def shrinkage(flows: dict[int, Flow]):
+        shrinkages: dict[int, list[tuple[str, float]]] = {}
+
+        for step, flow in flows.items():
+            shrinkages[step] = GradientFlowAnalyzer._shrinkage_flow(flow)
+        return shrinkages
+
+    @staticmethod
+    def _shrinkage_flow(flow: Flow) -> list[tuple[str, float]]:
+        shrinkages: list[tuple[str, float]] = []
+
+        for i, o in zip(flow[:-1], flow[1:]):
+            label = f"{i[0]} -> {o[0]}"
+            shrinkages.append((label, f"{(i[-1]/o[-1]):.3f}"))
+
+        return shrinkages